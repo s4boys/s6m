9262997ba3d0c6384986071d3acde9e7cacf0e1a
Nesuma <33174209+Nesuma@users.noreply.github.com>
2019-10-09 13:24:48 +0200

    Initial files

---
+++ ConcatenatedData.java
@@ -0,0 +1,53 @@
+package project_one;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
+import java.util.stream.Stream;
+
+import data.*;
+
+public class ConcatenatedData implements Datenbasis {
+
+	public String[] getBeobachtungsraum() {
+		String[] months_2012 = data_2012.getBeobachtungsraum();
+		String[] months_2013 = data_2013.getBeobachtungsraum();
+		String[] months_2014 = data_2014.getBeobachtungsraum();
+				
+		Stream<String> m12 = Arrays.stream(months_2012).map(a -> a.concat(", 2012"));
+		Stream<String> m13 = Arrays.stream(months_2013).map(a -> a.concat(", 2013"));
+		Stream<String> m14 = Arrays.stream(months_2014).map(a -> a.concat(", 2014"));
+		
+		return Stream.concat(m12,Stream.concat(m13, m14)).toArray(String[]::new);
+
+	}
+
+	public String getNameBeobachtungsraum() {
+		return "Monat";
+	}
+
+	public String getNameMerkmalsauspraegung() {
+		return "Heizwaermebedarf [MWh]";
+	}
+
+	public String getTopic() {
+		return "Wärmebedarf Bau2 aus den Jahren 2012/2013/2014";
+	}
+
+	public Number[] getMerkmalsauspraegungen() {
+		Number[] months_2012 = data_2012.getMerkmalsauspraegungen();
+		Number[] months_2013 = data_2013.getMerkmalsauspraegungen();
+		Number[] months_2014 = data_2014.getMerkmalsauspraegungen();
+				
+		Stream<Number> m12 = Arrays.stream(months_2012);
+		Stream<Number> m13 = Arrays.stream(months_2013);
+		Stream<Number> m14 = Arrays.stream(months_2014);
+
+		return Stream.concat(m12,Stream.concat(m13, m14)).toArray(Number[]::new);
+	}
+	
+	private Datenbasis data_2012 = new HeizwaermeBau2_2012();
+	private Datenbasis data_2013 = new HeizwaermeBau2_2013();
+	private Datenbasis data_2014 = new HeizwaermeBau2_2014();
+
+}
---
+++ PieChartTask.java
@@ -0,0 +1,50 @@
+package project_one;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import data.*;
+import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
+import javafx.stage.Stage;
+import project_one.*;
+
+public class PieChartTask extends Application{
+
+	private ObservableList<PieChart.Data> createPieDataSet(Datenbasis daten) {
+
+		ObservableList<PieChart.Data> list = 
+				FXCollections.observableArrayList();
+	
+		for (int i = 0; i < daten.getBeobachtungsraum().length; i++) {
+			list.add(new PieChart.Data(
+					daten.getBeobachtungsraum()[i], 
+					daten.getMerkmalsauspraegungen()[i].doubleValue()));
+		}
+
+		return list;
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+	    Datenbasis data = new ConcatenatedData();
+		
+	    ObservableList<PieChart.Data> pieChartData = createPieDataSet(data);
+	    
+    	// Erstellen und Beschriften des Diagramms
+       	final PieChart chart = new PieChart(pieChartData);
+        chart.setTitle(data.getTopic());
+        
+        // Rendern des Diagramms
+        Scene scene = new Scene(chart,888,666);
+        stage.setTitle("GVI Aufgabe 1: Tortendiagramm");
+        stage.setScene(scene);
+        stage.show();
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
